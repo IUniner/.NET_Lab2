23876ddc6dcce860c7ff977eca6c9b1b24958bb6
PREACHER <44905222+PRE4CHER@users.noreply.github.com>
2020-11-13 11:45:47 +0300

    FileWatcher v1.7

--- CA.NET_Lab2/Program.cs
+++ CA.NET_Lab2/Program.cs
@@ -14,16 +14,40 @@ namespace CA.NET_Lab2
         //System.Console.InputEncoding = Encoding.GetEncoding(1251);
         static FileInfo currentFile = null;
         static FileInfo currentArchive = null;
-        static string SourceDirectory = "C:\\ClientData\\";
-        static string TargetDirectory = "C:\\ServerData\\";
+        static string SourceDirectory = "C:\\ClientData\\data\\";
+        static string TargetDirectory = "C:\\ServerData\\archive\\";
         static DirectoryInfo DirGen = new DirectoryInfo("C:\\");
         static void Main(string[] args)
         {
-            
+
             //DirectoryInfo DirGen = new DirectoryInfo(path + "12\\");
             //FileSystemWatcher watcher = new FileSystemWatcher(path);
 
             using (FileSystemWatcher changeWatcher = new FileSystemWatcher(SourceDirectory))
+            {
+                List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+                watchers.Add(changeWatcher);
+                watchers.Add(new FileSystemWatcher(TargetDirectory));
+                //FileInfo selectFile = new FileInfo(e.FullPath)
+                watchers[0].EnableRaisingEvents = true;
+                watchers[0].IncludeSubdirectories = true;
+                watchers[1].EnableRaisingEvents = true;
+                watchers[1].IncludeSubdirectories = true;
+
+
+                watchers[0].Changed += changeWatcher_Changed;
+                watchers[0].Created += changeWatcher_Created;
+                watchers[0].Deleted += changeWatcher_Deleted;
+                watchers[0].Renamed += changeWatcher_Renamed;
+
+                watchers[1].Changed += changeWatcher_Changed;
+                watchers[1].Created += changeWatcher_Created;
+                watchers[1].Deleted += changeWatcher_Deleted;
+                watchers[1].Renamed += changeWatcher_Renamed;
+
+                Console.Read();
+            }
+            /*using (FileSystemWatcher changeWatcher = new FileSystemWatcher(TargetDirectory))
             {
                 //FileInfo selectFile = new FileInfo(e.FullPath)
                 changeWatcher.EnableRaisingEvents = true;
@@ -35,22 +59,38 @@ namespace CA.NET_Lab2
                 changeWatcher.Renamed += changeWatcher_Renamed;
 
                 Console.Read();
-            }            
+            }*/
         }
         static void changeWatcher_Renamed(object sender, RenamedEventArgs e)
         {
-            Console.WriteLine("File: {0} renamed to {1} at time: {2}", e.OldName, e.FullPath, DateTime.Now.ToLocalTime());
+            Console.WriteLine("File|> {0} renamed to {1} at time: {2}", e.OldName, e.FullPath, DateTime.Now.ToLocalTime());
+            fileHandler(e.FullPath);
         }
         static void changeWatcher_Deleted(object sender, FileSystemEventArgs e)
         {
-            Console.WriteLine("File: {0} deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime());
+            if (new FileInfo(e.FullPath).Extension != ".gz" & e.FullPath.IndexOf(SourceDirectory) != -1)
+            {
+                Console.WriteLine("File|> {0} deleted at time: {1}", e.FullPath, DateTime.Now.ToLocalTime());
+            }
         }
         static void changeWatcher_Created(object sender, FileSystemEventArgs e)
         {
+            if (new FileInfo(e.FullPath).Extension != ".gz" & e.FullPath.IndexOf(SourceDirectory) != -1)
+            {
+                Console.WriteLine("File|> {0} created at time: {1} {2}", e.FullPath, DateTime.Now.ToLocalTime(), DateTime.Now.Month);
+            }
+            fileHandler(e.FullPath);
+        }
+        static void changeWatcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("File|> {0} changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime());
+            fileHandler(e.FullPath);
+        }
+        static void fileHandler(string eFPath)
+        {
             try
             {
-                Console.WriteLine("File: {0} created at time: {1} {2}", e.FullPath, DateTime.Now.ToLocalTime(), DateTime.Now.Month);
-                if ((currentArchive = new FileInfo(e.FullPath)).Extension == ".txt")
+                if ((currentArchive = new FileInfo(eFPath)).Extension == ".txt" && eFPath.IndexOf(SourceDirectory) != -1)
                 {
                     currentArchive = Compress(currentArchive);
                     if (currentArchive.Extension == ".gz")
@@ -65,19 +105,15 @@ namespace CA.NET_Lab2
                         File.Delete(currentArchive.FullName);
                     }
                 }
+                if ((currentArchive = new FileInfo(eFPath)).Extension == ".gz" && eFPath.IndexOf(TargetDirectory) != -1) //e.FullPath.Substring(e.FullPath.IndexOf(TargetDirectory),TargetDirectory.Length) == TargetDirectory
+                {
+                    Decompress(new FileInfo(eFPath));
+                }
             }
-            catch(FileNotFoundException ex)
+            catch (FileNotFoundException ex)
             {
-                Console.WriteLine("changeWatcher_Created exeption: " + ex.Message);
+                Console.WriteLine("fileHandler exeption: " + ex.Message);
             }
-        }
-        static void changeWatcher_Changed(object sender, FileSystemEventArgs e)
-        {
-            Console.WriteLine("File: {0} changed at time: {1}", e.FullPath, DateTime.Now.ToLocalTime());
-        }
-        static void sendFile(FileInfo currentFile)
-        {
-
         }
 
         static FileInfo Compress(FileInfo fileToCompress)
@@ -87,8 +123,8 @@ namespace CA.NET_Lab2
                 currentFile = fileToCompress;
                 using (FileStream originalFileStream = fileToCompress.OpenRead())
                 {
-                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden) 
-                        != FileAttributes.Hidden & 
+                    if ((File.GetAttributes(fileToCompress.FullName) & FileAttributes.Hidden)
+                        != FileAttributes.Hidden &
                        fileToCompress.Extension != ".gz")
                     {
                         using (FileStream compressedFileStream = File.Create(fileToCompress.FullName.Replace(".txt", ".gz")))
@@ -106,7 +142,37 @@ namespace CA.NET_Lab2
             catch (FileNotFoundException ex)
             {
                 Console.WriteLine("Compress error:" + ex.Message);
-                return currentFile;
+                return fileToCompress;
+            }
+        }
+        static FileInfo Decompress(FileInfo fileToDecompress)
+        {
+            try
+            {
+                currentArchive = fileToDecompress;
+                if (fileToDecompress.Extension == ".gz")
+                {
+                    using (FileStream originalFileStream = fileToDecompress.OpenRead())
+                    {
+                        //string currentFileName = fileToDecompress.FullName;
+                        //string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+                        using (FileStream decompressedFileStream = File.Create(fileToDecompress.FullName.Replace(".gz", ".txt")))
+                        {
+                            using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                            {
+                                decompressionStream.CopyTo(decompressedFileStream);
+                            }
+                        }
+                        currentArchive = new FileInfo(fileToDecompress.FullName.Replace(".gz", ".txt"));
+                        fileToDecompress.Delete();
+                    }
+                }
+                return currentArchive;
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Decompress error:" + ex.Message);
+                return fileToDecompress;
             }
         }
     }
